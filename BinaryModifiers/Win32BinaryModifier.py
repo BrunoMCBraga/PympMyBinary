e56282f5006bf6239554bfcff8fec44291833509
bruno.braga <bruno.mh.braga@gmail.com>
2017-03-07 16:41:53 +0000

    Imported Table adjustments implemented.

--- BinaryModifiers/Win32BinaryModifier.py
+++ BinaryModifiers/Win32BinaryModifier.py
@@ -135,6 +135,9 @@ class Win32BinaryModifier:
         aligned_size_of_image = unaligned_size_of_image + (section_alignment - (unaligned_size_of_image % section_alignment))
         MultiByteHandler.set_dword_given_offset(self.binary, size_of_image_offset, aligned_size_of_image)
 
+    '''
+        TODO: Consider the size for the resource table.
+    '''
     def adjust_resource_table(self):
         offset_to_resource_table_rva_within_header = self.header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_RESOURCE_TABLE_RVA
         resource_table_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_resource_table_rva_within_header)
@@ -167,21 +170,71 @@ class Win32BinaryModifier:
         #Adjusting RVA on Data Directories header.
         MultiByteHandler.set_dword_given_offset(self.binary, offset_to_resource_table_rva_within_header, resource_table_rva + len(self.shell_code))
 
+    '''
+        1.Get size of import table. Compute number of entries.
+        2.Adust RVAs within Import directory table:
+            -Adjust import name table RVA
+            -Adjust Name RVA
+            -Adjust Import address table RVA
+
+    '''
+    def adjust_import_table(self):
+        offset_to_import_table_rva = self.header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_IMPORT_TABLE_RVA
+        import_table_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_import_table_rva)
+        import_table_raw = Win32BinaryUtils.convert_rva_to_raw(self.binary, self.header_offset, import_table_rva)
+
+
+        current_import_directory_table_entry = import_table_raw
+
+
+        while(True):
+            if not Win32BinaryUtils.is_end_of_import_directory_table(self.binary, current_import_directory_table_entry):
+                break
+
+            offset_to_import_name_table_rva = current_import_directory_table_entry + Win32BinaryOffsetsAndSizes.OFFSET_TO_IMPORT_NAME_TABLE_RVA_WITHIN_IMPORT_DIRECTORY_TABLE
+            import_name_table_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_import_name_table_rva)
+            if import_name_table_rva != 0x0:
+                MultiByteHandler.set_dword_given_offset(self.binary, offset_to_import_name_table_rva, import_name_table_rva + len(self.shell_code))
+
+            offset_to_name_rva = current_import_directory_table_entry + Win32BinaryOffsetsAndSizes.OFFSET_TO_NAME_RVA_WITHIN_IMPORT_DIRECTORY_TABLE
+            name_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_name_rva)
+            if name_rva != 0x0:
+                MultiByteHandler.set_dword_given_offset(self.binary, offset_to_name_rva, name_rva + len(self.shell_code))
+
+            offset_to_import_address_table_rva = current_import_directory_table_entry + Win32BinaryOffsetsAndSizes.OFFSET_TO_IMPORT_ADDRESS_TABLE_RVA_WITHIN_IMPORT_DIRECTORY_TABLE
+            import_address_table_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_import_address_table_rva)
+            if import_address_table_rva != 0x0:
+                raw_offset_for_import_address_table = Win32BinaryUtils.convert_rva_to_raw(self.binary,self.header_offset,import_address_table_rva)
+                while True:
+                    hint_name_rva_or_ordinal = MultiByteHandler.get_dword_given_offset(self.binary,raw_offset_for_import_address_table)
+                    if hint_name_rva_or_ordinal == 0x0:
+                        break
+                    if (0x80000000 & hint_name_rva_or_ordinal) != 0x80000000:
+                        MultiByteHandler.set_dword_given_offset(self.binary, raw_offset_for_import_address_table, hint_name_rva_or_ordinal + len(self.shell_code))
+                    raw_offset_for_import_address_table += 0x4 #Each hint/name/ordinal takes 4 bytes (dword)
+
+                MultiByteHandler.set_dword_given_offset(self.binary, offset_to_import_address_table_rva, import_address_table_rva + len(self.shell_code))
+
+            current_import_directory_table_entry += Win32BinaryOffsetsAndSizes.NUMBER_OF_BYTES_PER_IMPORT_DIRECTORY_TABLE_ENTRY
+
+        # Adjusting RVA on Data Directories header.
+        MultiByteHandler.set_dword_given_offset(self.binary, offset_to_import_table_rva, import_table_rva + len(self.shell_code))
+
     def adjust_data_directories(self):
-
-        entrypoint_rva_offset_within_header = self.header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_ENTRYPOINT_RVA
-        entrypoint_rva = MultiByteHandler.get_dword_given_offset(self.binary, entrypoint_rva_offset_within_header)
-        entrypoint_section_header_index_and_offset = Win32BinaryUtils.get_raw_offset_for_header_of_section_containing_given_rva(self.binary, self.header_offset, entrypoint_rva)
-        offset_of_header_of_section_containing_entrypoint = entrypoint_section_header_index_and_offset[1]
-
-        entrypoint_virtual_section_rva_offset = offset_of_header_of_section_containing_entrypoint + Win32BinaryOffsetsAndSizes.OFFSET_TO_SECTION_VIRTUAL_ADDRESS_WITHIN_SECTION_HEADER
-        entrypoint_virtual_section_rva = MultiByteHandler.get_dword_given_offset(self.binary, entrypoint_virtual_section_rva_offset)
 
         offset_to_resource_section_rva_within_header = self.header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_RESOURCE_TABLE_RVA
         resource_section_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_resource_section_rva_within_header)
 
-        if resource_section_rva > entrypoint_virtual_section_rva:
+        if resource_section_rva != 0x0 and Win32BinaryUtils.rva_requires_change(self.binary, self.header_offset, resource_section_rva):
             self.adjust_resource_table()
+
+        offset_to_import_table_rva_within_header = self.header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_IMPORT_TABLE_RVA
+        import_table_rva = MultiByteHandler.get_dword_given_offset(self.binary, offset_to_import_table_rva_within_header)
+
+        if import_table_rva != 0x0 and Win32BinaryUtils.rva_requires_change(self.binary, self.header_offset, import_table_rva):
+            self.adjust_import_table()
+
+
 
     '''
         1.Modify Virtual and Raw sizes of section containing shellcode
--- Utils/Win32BinaryUtils.py
+++ Utils/Win32BinaryUtils.py
@@ -24,14 +24,41 @@ class Win32BinaryUtils:
             current_header_offset += Win32BinaryOffsetsAndSizes.SECTION_HEADER_SIZE
 
     @staticmethod
-    def convert_rva_to_raw(binary_data, header_offset, rva):
-        section_header_offset = Win32BinaryUtils.get_raw_offset_for_header_of_section_containing_given_rva(binary_data, header_offset, rva)[1]
+    def convert_rva_to_raw(binary, header_offset, rva):
+        section_header_offset = Win32BinaryUtils.get_raw_offset_for_header_of_section_containing_given_rva(binary, header_offset, rva)[1]
 
         virtual_section_rva_offset = section_header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_SECTION_VIRTUAL_ADDRESS_WITHIN_SECTION_HEADER
-        virtual_section_rva = MultiByteHandler.get_dword_given_offset(binary_data, virtual_section_rva_offset)
+        virtual_section_rva = MultiByteHandler.get_dword_given_offset(binary, virtual_section_rva_offset)
 
         raw_section_offset_offset = section_header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_SECTION_RAW_ADDRESS_WITHIN_SECTION_HEADER
-        raw_section_offset = MultiByteHandler.get_dword_given_offset(binary_data, raw_section_offset_offset)
+        raw_section_offset = MultiByteHandler.get_dword_given_offset(binary, raw_section_offset_offset)
 
         return rva - virtual_section_rva + raw_section_offset
+
+    @staticmethod
+    def rva_requires_change(binary, header_offset,  rva):
+
+        entrypoint_rva_offset_within_header = header_offset + Win32BinaryOffsetsAndSizes.OFFSET_TO_ENTRYPOINT_RVA
+        entrypoint_rva = MultiByteHandler.get_dword_given_offset(binary, entrypoint_rva_offset_within_header)
+        entrypoint_section_header_index_and_offset = Win32BinaryUtils.get_raw_offset_for_header_of_section_containing_given_rva(binary, header_offset, entrypoint_rva)
+        offset_of_header_of_section_containing_entrypoint = entrypoint_section_header_index_and_offset[1]
+
+        entrypoint_virtual_section_rva_offset = offset_of_header_of_section_containing_entrypoint + Win32BinaryOffsetsAndSizes.OFFSET_TO_SECTION_VIRTUAL_ADDRESS_WITHIN_SECTION_HEADER
+        entrypoint_virtual_section_rva = MultiByteHandler.get_dword_given_offset(binary, entrypoint_virtual_section_rva_offset)
+
+        if rva > entrypoint_virtual_section_rva:
+            return True
+
+    #Import address table endes with
+    @staticmethod
+    def is_end_of_import_directory_table(binary, offset):
+        and_dwords = 0x0
+
+        for dword_within_directory_table_entry in range(0, Win32BinaryOffsetsAndSizes.NUMBER_OF_DWORDS_WITHIN_EACH_DIRECTORY_TABLE_ENTRY):
+            and_dwords += MultiByteHandler.get_dword_given_offset(binary, offset)
+
+        if and_dwords != 0x0:
+            False
+        else:
+            return True
 
